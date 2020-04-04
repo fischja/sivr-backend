50c4e4670f7998289e54804a96895630ee48e56f
Jan <fischja10@gmail.com>
2020-04-04 09:54:16 +0200

    added basic meta data text search

---
+++ sivr-backend/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sivr_backend
+{
+    public static class Extensions
+    {
+        public static int CountSubstring(this string text, string value)
+        {
+            int count = 0, minIndex = text.IndexOf(value, 0);
+            while (minIndex != -1)
+            {
+                minIndex = text.IndexOf(value, minIndex + value.Length);
+                count++;
+            }
+            return count;
+        }
+    }
+}
--- sivr-backend/Repositories/MetaDataRepository.cs
+++ sivr-backend/Repositories/MetaDataRepository.cs
@@ -3,6 +3,8 @@ using sivr_backend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace sivr_backend.Repositories
@@ -18,8 +20,43 @@ namespace sivr_backend.Repositories
 
         public async Task<IList<MetaData>> Get(string text)
         {
-            return await _context.MetaDatas.ToListAsync();
+            return _context.MetaDatas
+                .AsEnumerable()
+                .Select(x => new
+                {
+                    NumOccurences = x.Title.CountSubstring(text)
+                                    + x.FullTitle.CountSubstring(text)
+                                    + x.Description.CountSubstring(text)
+                                    + x.Licence.CountSubstring(text)
+                                    + x.Extractor.CountSubstring(text)
+                                    + x.ExtractorKey.CountSubstring(text)
+                                    + x.SourceOrigFilename.CountSubstring(text)
+                                    + x.DisplayId.CountSubstring(text)
+                                    + x.WebpageUrl.CountSubstring(text)
+                                    + x.WebpageUrlBasename.CountSubstring(text)
+                                    + x.Thumbnail.CountSubstring(text)
+                                    + x.Format.CountSubstring(text)
+                                    + x.FormatId.CountSubstring(text)
+                                    + x.Resolution.CountSubstring(text)
+                                    + x.StretchedRatio.CountSubstring(text)
+                                    + x.Fps.CountSubstring(text)
+                                    + x.Abr.CountSubstring(text)
+                                    + x.Ext.CountSubstring(text)
+                                    + x.Vbr.CountSubstring(text)
+                                    + x.ACodec.CountSubstring(text)
+                                    + x.VCodec.CountSubstring(text)
+                                    + x.Channel.CountSubstring(text)
+                                    + x.Uploader.CountSubstring(text)
+                                    + x.UploaderId.CountSubstring(text)
+                                    + x.UploaderUrl.CountSubstring(text)
+                                    + x.Playlist.CountSubstring(text),
+                    Obj = x
+                })
+                .OrderByDescending(x => x.NumOccurences)
+                // .Take(10)
+                .Where(x => x.NumOccurences > 0)
+                .Select(x => x.Obj)
+                .ToList();
         }
-
     }
 }

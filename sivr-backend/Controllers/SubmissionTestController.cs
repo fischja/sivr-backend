6d59e7c57f47840f86b62de6e6c2215faab79539
Jan <fischja10@gmail.com>
2020-05-03 04:04:46 +0200

    added submission test controller

---
+++ sivr-backend/Controllers/SubmissionTestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sivr_backend.Controllers
+{
+    [Route("vbs/submit/")]
+    [ApiController]
+    public class SubmissionTestController : Controller
+    {
+        [HttpPost]
+        public ActionResult Post(
+            [FromQuery] int team,
+            [FromQuery] int member,
+            [FromQuery] int video,
+            [FromQuery] int frame)
+        {
+            return Ok();
+        }
+    }
+}
